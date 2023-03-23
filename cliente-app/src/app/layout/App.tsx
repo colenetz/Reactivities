a7735739b4bd43d76d6195bcfe916436a73b3529
colenetz <carloscolenetz@gmail.com>
2023-03-22 21:09:30 -0300

    end of section 5

--- cliente-app/src/app/layout/App.tsx
+++ cliente-app/src/app/layout/App.tsx
@@ -1,15 +1,16 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import './styles.css';
 import axios from 'axios'
-import { Container, Header } from 'semantic-ui-react';
-import List from 'semantic-ui-react/dist/commonjs/elements/List';
-import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
+import {Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined); 
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(()=>{
     axios.get<Activity[]>('http://localhost:5000/api/activities')
@@ -19,11 +20,48 @@ function App() {
     })
   },[])
 
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x=>x.id === id));
+  }
+
+  function handleCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string){
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity){
+    activity.id ? setActivities([...activities.filter(x=> x.id !== activity.id), activity])
+    : setActivities([...activities, {...activity, id: uuid()}]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string){
+    setActivities([...activities.filter(x=> x.id !== id)])
+  }
+
   return (
     <Fragment>
-        <NavBar />
+        <NavBar openForm={handleFormOpen}/>
         <Container style={{marginTop:'7em'}}>
-          <ActivityDashboard activities={activities} />
+          <ActivityDashboard activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEditActivity={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
+          />
         </Container>
     </Fragment>
   );
--- cliente-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ cliente-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,17 +1,46 @@
+import { create } from 'domain';
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
 interface Props{
     activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity : (id: string) => void;
+    cancelSelectActivity : () => void;
+    editMode: boolean;
+    openForm:(id: string) => void;
+    closeForm: () => void;
+    createOrEditActivity: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityDashboard({activities}: Props){
+export default function ActivityDashboard({activities, selectedActivity, deleteActivity,
+    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEditActivity}: Props){
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities}/>
+                <ActivityList activities={activities}
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
+                />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode &&
+                <ActivityDetails 
+                    activity={selectedActivity} 
+                    cancelSelectActivity={cancelSelectActivity}
+                    openForm={openForm}
+                />}
+                {editMode &&
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity}
+                    createOrEditActivity={createOrEditActivity}
+                />}
             </Grid.Column>
         </Grid>
     )
